3667bca84689fa95b4c3202a625effc205ca02a6
Viktor Starodubtsev <viktorst2005@gmail.com>
2025-03-20 22:23:04 +0400

    Fixes broken pixels.

--- src/image_processing.rs
+++ src/image_processing.rs
@@ -6,13 +6,14 @@ use std::path::PathBuf;
 pub fn rgb_to_grayscale(path: PathBuf) -> (u32, u32, Bytes) {
     println!("Path in rgb_to_grayscale {:?}", path);
     let img_buf = open(path).unwrap_or_default().into_rgb8();
+    let (width, height) = (img_buf.width(), img_buf.height());
 
     println!("ImageBuffer Size {}", img_buf.as_bytes().len());
+    let rgba_pixels: Vec<u8> = img_buf
+        .pixels()
+        .flat_map(|p| [p.0[0], p.0[1], p.0[2], 255]) // Add alpha=255
+        .collect();
+    // println!("Pixels: {:?}", rgba_pixels);
 
-    let width = img_buf.width();
-    let height = img_buf.height();
-    let pixels = img_buf.into_vec();
-    println!("Pixels: {:?}", pixels);
-
-    (width, height, pixels.into())
+    (width, height, Bytes::from(rgba_pixels))
 }
