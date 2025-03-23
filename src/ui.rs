becbed571fc4ec71cd74b3f448aeeae1ffabf371
Viktor Starodubtsev <viktorst2005@gmail.com>
2025-03-24 01:38:31 +0400

    Corrects UI.

--- src/ui.rs
+++ src/ui.rs
@@ -42,14 +42,6 @@ impl UIState {
                     .unwrap_or_default();
                 self.handle_rgba_components =
                     HandleRgbaComponents::from_rgb_to_greyscale(self.image_path.clone().into());
-                // self.handle_rgba_components = HandleRgbaComponents::greyscale_to_brightness_slice(
-                //     &self.handle_rgba_components,
-                //     self.min_brightness.parse().unwrap_or(0),
-                //     self.max_brightness.parse().unwrap_or(255),
-                // );
-                self.handle_rgba_components =
-                    HandleRgbaComponents::prewitt_filtered(&self.handle_rgba_components);
-
                 Task::none()
             }
             Message::MinBrightnessChange(text) => {
@@ -70,14 +62,27 @@ impl UIState {
 
         column![
             text("Trdelniki").size(21).width(WINDOW_WIDTH).center(),
-            center(self.greyscale_image(iced_image::FilterMethod::Nearest))
-                .style(container::bordered_box),
-            button(text("Upload File")).on_press(Message::UploadImage),
             row![
-                text_input("min brightness", self.min_brightness.as_ref())
-                    .on_input(Message::MinBrightnessChange),
-                text_input("max brightness", self.max_brightness.as_ref())
-                    .on_input(Message::MaxBrightnessChange)
+                self.element_image_original(),
+                self.element_image_greyscale()
+            ],
+            row![
+                self.element_image_brightness_slice_keep_bg(),
+                self.element_image_prewitt_filtered()
+            ],
+            button(text("Upload File")).on_press(Message::UploadImage),
+            text("Параметры яркостного среза:"),
+            row![
+                text_input(
+                    "Минимальная яркость (по умолчанию 0)",
+                    self.min_brightness.as_ref()
+                )
+                .on_input(Message::MinBrightnessChange),
+                text_input(
+                    "Максимальная яркость (по умолчанию 255)",
+                    self.max_brightness.as_ref()
+                )
+                .on_input(Message::MaxBrightnessChange)
             ],
         ]
         .spacing(10)
@@ -89,7 +94,42 @@ impl UIState {
         Theme::GruvboxLight
     }
 
-    fn greyscale_image<'a>(&self, filter_method: iced_image::FilterMethod) -> Viewer<Handle> {
+    fn element_image_original(&self) -> Element<Message> {
+        column![
+            center(iced::widget::image(self.image_path.clone())).style(container::bordered_box),
+            text("Оригинал изображения")
+        ]
+        .into()
+    }
+
+    fn element_image_greyscale(&self) -> Element<Message> {
+        column![
+            center(self.image_greyscale(iced_image::FilterMethod::Nearest))
+                .style(container::bordered_box),
+            text("Изображение в оттенках серого")
+        ]
+        .into()
+    }
+
+    fn element_image_brightness_slice_keep_bg(&self) -> Element<Message> {
+        column![
+            center(self.image_brightness_slice_keep_bg(iced_image::FilterMethod::Nearest))
+                .style(container::bordered_box),
+            text("Яркостный срез с сохранением фона")
+        ]
+        .into()
+    }
+
+    fn element_image_prewitt_filtered(&self) -> Element<Message> {
+        column![
+            center(self.image_prewitt_filtered(iced_image::FilterMethod::Nearest))
+                .style(container::bordered_box),
+            text("Изображение под x-Prewitt-фильтром")
+        ]
+        .into()
+    }
+
+    fn image_greyscale<'a>(&self, filter_method: iced_image::FilterMethod) -> Viewer<Handle> {
         let handle = Handle::from_rgba(
             self.handle_rgba_components.width,
             self.handle_rgba_components.height,
@@ -98,8 +138,55 @@ impl UIState {
         println!("Handle {:#?}", handle);
 
         let img = iced::widget::image::viewer(handle)
-            .width(WINDOW_WIDTH)
-            .height(VIEWER_HEIGHT)
+            .width(WINDOW_WIDTH / 3.0)
+            .height(VIEWER_HEIGHT / 3.0)
+            .filter_method(filter_method);
+
+        img
+    }
+
+    fn image_brightness_slice_keep_bg<'a>(
+        &self,
+        filter_method: iced_image::FilterMethod,
+    ) -> Viewer<Handle> {
+        let handle = Handle::from_rgba(
+            self.handle_rgba_components.width,
+            self.handle_rgba_components.height,
+            self.handle_rgba_components
+                .greyscale_to_brightness_slice_keep_bg(
+                    self.min_brightness.parse().unwrap_or_default(),
+                    self.max_brightness.parse().unwrap_or_default(),
+                )
+                .pixels
+                .clone(),
+        );
+        println!("Handle {:#?}", handle);
+
+        let img = iced::widget::image::viewer(handle)
+            .width(WINDOW_WIDTH / 3.0)
+            .height(VIEWER_HEIGHT / 3.0)
+            .filter_method(filter_method);
+
+        img
+    }
+
+    fn image_prewitt_filtered<'a>(
+        &self,
+        filter_method: iced_image::FilterMethod,
+    ) -> Viewer<Handle> {
+        let handle = Handle::from_rgba(
+            self.handle_rgba_components.width,
+            self.handle_rgba_components.height,
+            self.handle_rgba_components
+                .prewitt_filtered()
+                .pixels
+                .clone(),
+        );
+        println!("Handle {:#?}", handle);
+
+        let img = iced::widget::image::viewer(handle)
+            .width(WINDOW_WIDTH / 3.0)
+            .height(VIEWER_HEIGHT / 3.0)
             .filter_method(filter_method);
 
         img
